777e7a42d9b33e0385dcabc5e5d0c2f53b8f9448
Sebastian <sebaj348@gmail.com>
2026-03-12 18:38:36 -0400

    update

--- src/controllers/Cl_cGrupo.ts
+++ src/controllers/Cl_cGrupo.ts
@@ -1,3 +1,14 @@
+// Sea el nombre y la edad de varias personas.
+// Se necesita un programa que lea estos datos y reporte
+// al final el porcentaje de personas que son mayores de edad.
+// Se tienen las siguientes personas: Luis (15), Ana (19),
+//  José (21), Carmen (17), Rosa (18), José (22), María (17),
+// Luz (19), Rafael (23), Liz (15), Marcos (20) y Leo (16);
+// la salida requerida presenta el siguiente formato:
+// Cantidad de personas: 12
+// Cantidad de personas mayor de edad: 7
+// Porcentaje de personas mayor de edad: 58.33%
+
 import Cl_mGrupo from "../models/Cl_mGrupo.js";
 import Cl_vGrupo from "../views/Cl_vGrupo.js";
 import Cl_cPersona from "./Cl_cPersona.js";
